47d7c51bb80bab59b45154fa35afef65c36163de
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2024-12-21 19:07:57 +0545

    Nav resizable facility
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- src/components/core/layout/AppNav.vue
+++ src/components/core/layout/AppNav.vue
@@ -1,79 +1,100 @@
 <template>
-  <nav class="app_nav">
-    <header class="d-flex justify-space-between ga-2 pa-2">
-      <button
-        class="flex-centered ga-2 menu-btn"
-      >
-        <v-tooltip
-          activator="parent"
-          location="top"
+  <div class="d-flex">
+    <nav class="app_nav flex-grow-1"
+      :style="{
+        width: width + 'px'
+      }"
+    >
+      <header class="d-flex justify-space-between ga-2 pa-2">
+        <button
+          class="flex-centered ga-2 menu-btn"
         >
-          Collapse Your Library
-        </v-tooltip>
-        <VImg
-          src="@/assets/icons/svgs/book.svg"
-          height="24"
-          width="24"
+          <v-tooltip
+            activator="parent"
+            location="top"
+          >
+            Collapse Your Library
+          </v-tooltip>
+          <VImg
+            src="@/assets/icons/svgs/book.svg"
+            height="24"
+            width="24"
+          />
+          Your Library
+        </button>
+        <button class="icon-btn">
+          <v-tooltip
+            activator="parent"
+            location="top"
+          >
+            Create playlist or folder
+          </v-tooltip>
+          <VImg
+            src="@/assets/icons/svgs/plus.svg"
+            height="24"
+            width="24"
+          />
+        </button>
+      </header>
+
+      <div class="d-flex flex-column ga-2 flex-grow-1 pt-12">
+        <NavCard
+          title="Create your first playlist"
+          subtitle="It's easy we will help you"
+          buttonText="Create playlist"
         />
-        Your Library
-      </button>
-      <button class="icon-btn">
-        <v-tooltip
-          activator="parent"
-          location="top"
-        >
-          Create playlist or folder
-        </v-tooltip>
-        <VImg
-          src="@/assets/icons/svgs/plus.svg"
-          height="24"
-          width="24"
+
+        <NavCard
+          title="Let's find some podcasts to follow"
+          subtitle="We will keep you updated on new episodes"
+          buttonText="Browse podcasts"
         />
-      </button>
-    </header>
+      </div>
 
-    <div class="d-flex flex-column ga-2 flex-grow-1 pt-12">
-      <NavCard
-        title="Create your first playlist"
-        subtitle="It's easy we will help you"
-        buttonText="Create playlist"
-      />
+      <div class="nav-links ml-4">
+        <ul class="ga-2">
+          <li
+            v-for="menu in menus"
+            :key="menu.label"
+          >
+            <a :href="menu.href">{{ menu.label }}</a>
+          </li>
+        </ul>
 
-      <NavCard
-        title="Let's find some podcasts to follow"
-        subtitle="We will keep you updated on new episodes"
-        buttonText="Browse podcasts"
-      />
-    </div>
+        <a href="#" class="mb-6">Cookies</a>
+      </div>
 
-    <div class="nav-links ml-4">
-      <ul class="ga-2">
-        <li
-          v-for="menu in menus"
-          :key="menu.label"
-        >
-          <a :href="menu.href">{{ menu.label }}</a>
-        </li>
-      </ul>
-
-      <a href="#" class="mb-6">Cookies</a>
-    </div>
-
-    <div class="mb-4 ml-4">
-      <button class="outline-btn">
-        <img
-          src="@/assets/icons/svgs/language.svg"
-          height="24"
-          width="24"
-          alt="Language"
-        >
-        English
-      </button>
-    </div>
-  </nav>
+      <div class="mb-4 ml-4">
+        <button class="outline-btn">
+          <img
+            src="@/assets/icons/svgs/language.svg"
+            height="24"
+            width="24"
+            alt="Language"
+          >
+          English
+        </button>
+      </div>
+    </nav>
+    <div
+      :class="{
+        'resizer ease-transition': true,
+        'allowed-width': isValidWidth,
+        'grabbing': grabbing
+      }"
+      @mousedown="handleMouseDown"
+    />
+  </div>
 </template>
 <script setup>
+import { ref, computed } from "vue";
 import NavCard from "@/components/core/layout/NavCard.vue";
+
+const maxWidth = 500;
+const minWidth = 250;
+const width = ref(420)
+
+const isValidWidth = computed(() => width.value >= minWidth && width.value <= maxWidth);
 
 const menus = [
   {
@@ -101,6 +122,35 @@ const menus = [
     href: "#"
   },
 ]
+
+const grabbing = ref(false);
+const handleMouseDown = (e) => {
+  e.preventDefault();
+  grabbing.value = true;
+  window.addEventListener("mousemove", handleMouseMove);
+  window.addEventListener("mouseup", handleMouseUp);
+}
+
+const handleMouseMove = (e) => {
+  if (e.clientX >= minWidth && e.clientX <= maxWidth) {
+    width.value = e.clientX;
+  }
+}
+
+const handleMouseUp = () => {
+  grabbing.value = false;
+  window.removeEventListener("mousemove", handleMouseMove);
+  window.removeEventListener("mouseup", handleMouseUp);
+}
+
+watch(grabbing, (value) => {
+  if (value) {
+    document.body.style.cursor = "ew-resize";
+  } else {
+    document.body.style.cursor = "default";
+  }
+})
+
 </script>
 <style lang="sass" scoped>
 .app_nav
@@ -128,4 +178,14 @@ const menus = [
     padding: 4px 8px
     font-size: 0.875rem
     font-weight: 600
+
+.resizer
+  height: 100%
+  padding: 0 1.5px
+  &:active
+    background-color: rgba(255, 255, 255, 0.3)
+  &.allowed-width
+    cursor: ew-resize
+    &:hover
+      background-color: rgba(255, 255, 255, 0.2)
 </style>
