8e2217860696d9fb842e1d088ba27eb98acb401a
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2024-12-25 00:04:09 +0545

    Play button popup menu impl
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- src/components/core/home/PlayBtn.vue
+++ src/components/core/home/PlayBtn.vue
@@ -1,24 +1,76 @@
 <template>
-  <button
-    :class="{
-      'play-button': true,
-      [className]: true,
-      'hidden-initial' : className === ''
-    }"
+  <VDialog
+    max-width="820"
+    class="play-btn-dialog"
   >
-    <img
-      src="@/assets/icons/svgs/play.svg"
-      alt="play"
-    >
-  </button>
+    <template #activator="{props}">
+      <button
+        :class="{
+          'play-button': true,
+          [className]: true,
+          'hidden-initial' : className === ''
+        }"
+        v-bind="props"
+      >
+        <img
+          src="@/assets/icons/svgs/play.svg"
+          alt="play"
+        >
+      </button>
+    </template>
+
+    <VCard>
+      <div
+        class="play-btn-dialog-content"
+        :style="{
+          background: headerSectionBg
+        }"
+      >
+        <div class="play-btn-dialog-content--image">
+          <CustomImage
+            :src="resource.image"
+            alt="play"
+          />
+        </div>
+        <div class="play-btn-dialog-content--info">
+          <h3>Start listening with a free Spotify account</h3>
+
+          <div class="actions">
+            <button class="filled-btn">
+              Sign up free
+            </button>
+            <button class="outline-btn">
+              Download App
+            </button>
+          </div>
+
+          <p>
+            Already have an account? <a href="#">Log in</a>
+          </p>
+        </div>
+      </div>
+    </VCard>
+  </VDialog>
 </template>
 <script setup>
+import {faker} from "@faker-js/faker";
+import useGradientFromImage from "@/composables/useGradientFromImage.js";
+import CustomImage from "@/components/designComponents/CustomImage.vue";
+
 defineProps({
   className: {
     type: String,
     default: ""
   }
 })
+
+const resource = {
+  image: faker.image.urlPicsumPhotos({blur: 0})
+}
+
+const {
+  headerSectionBg
+} = useGradientFromImage(resource.image);
 </script>
 <style lang="scss" scoped>
 .play-button {
@@ -44,6 +96,59 @@ defineProps({
     opacity: 0;
     transform: scale(0.8);
     transition: opacity 0.3s ease, transform 0.3s ease;
+  }
+}
+
+.play-btn-dialog-content {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 4rem;
+  gap: 4rem;
+
+  background: #121212;
+
+  .play-btn-dialog-content--image {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      height: 100%;
+      width: 100%;
+      aspect-ratio: 1;
+      border-radius: 8px !important;
+    }
+  }
+
+  .play-btn-dialog-content--info {
+    flex: 2;
+    padding: 0 1rem;
+    text-align: center;
+
+    h3 {
+      font-size: 1.8rem;
+      margin-bottom: 2rem;
+    }
+
+    .actions {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: .2rem;
+      button {
+        width: fit-content;
+        padding: 1rem 2rem;
+        margin-bottom: 1rem;
+      }
+    }
+
+    p {
+      margin-top: 1rem;
+      a {
+        color: var(--v-theme-play);
+      }
+    }
   }
 }
 </style>
